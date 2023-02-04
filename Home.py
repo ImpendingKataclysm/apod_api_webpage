10dd0b5062fdb37b676b316cda15fe387b46dc8c
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-04 11:29:02 -0800

    Created streamlit web page that displays an image and text

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+img_url = "photo.png"
+page_text = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco.
+"""
+
+st.image(img_url)
+st.write(page_text)
