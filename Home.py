f44f79fcadf46eda2a48d69d9cd78fd2024a8eeb
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-05 10:43:21 -0800

    Write image to file when program runs, add image file to gitignore

--- Home.py
+++ Home.py
@@ -4,13 +4,21 @@ from requests import get
 api_key = "sIwzWUH1dNgBnD9M8yYT8xDXc9OpQwgBCYuC6eUj"
 request_url = "https://api.nasa.gov/planetary/apod?" + \
               f"api_key={api_key}"
-request = get(request_url)
-content = request.json()
 
-img_url = content["hdurl"]
+response = get(request_url)
+content = response.json()
+
+title = content["title"]
+img_url = content["url"]
 page_text = content["explanation"]
 
-st.title(content["title"])
+img_path = "img.png"
+img_response = get(img_url)
+
+with open(img_path, "wb") as file:
+    file.write(img_response.content)
+
+st.title(title)
 st.subheader("Astronomy Picture of the Day")
-st.image(img_url)
+st.image(img_path)
 st.write(page_text)
